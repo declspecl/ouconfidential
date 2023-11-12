1dd855b8d016d336ffd0390009c40983d5888838
declspecl <gavind2559@gmail.com>
2023-11-12 00:26:24 -0500

    set up tailwind and main.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,83 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/**/*.{ts,tsx}"
+    ],
+    theme: {
+        colors: {
+            "text": {
+                "DEFAULT": "hsl(var(--text))",
+                50: "hsl(var(--text-50))",
+                100: "hsl(var(--text-100))",
+                150: "hsl(var(--text-150))",
+                200: "hsl(var(--text-200))",
+                250: "hsl(var(--text-250))",
+                300: "hsl(var(--text-300))"
+            },
+
+            "background": {
+                "DEFAULT": "hsl(var(--background))",
+                50: "hsl(var(--background-50))",
+                100: "hsl(var(--background-100))",
+                150: "hsl(var(--background-150))",
+                200: "hsl(var(--background-200))",
+                250: "hsl(var(--background-250))",
+                300: "hsl(var(--background-300))"
+            },
+
+            "gold": {
+                "DEFAULT": "hsl(var(--gold-500))",
+                0: "hsl(var(--gold-0))",
+                100: "hsl(var(--gold-100))",
+                200: "hsl(var(--gold-200))",
+                300: "hsl(var(--gold-300))",
+                400: "hsl(var(--gold-400))",
+                500: "hsl(var(--gold-500))",
+                600: "hsl(var(--gold-600))",
+                700: "hsl(var(--gold-700))",
+                800: "hsl(var(--gold-800))",
+                900: "hsl(var(--gold-900))",
+                1000: "hsl(var(--gold-1000))",
+            },
+
+            "blue": {
+                "DEFAULT": "hsl(var(--blue-500))",
+                0: "hsl(var(--blue-0))",
+                100: "hsl(var(--blue-100))",
+                200: "hsl(var(--blue-200))",
+                300: "hsl(var(--blue-300))",
+                400: "hsl(var(--blue-400))",
+                500: "hsl(var(--blue-500))",
+                600: "hsl(var(--blue-600))",
+                700: "hsl(var(--blue-700))",
+                800: "hsl(var(--blue-800))",
+                900: "hsl(var(--blue-900))",
+                1000: "hsl(var(--blue-1000))"
+            },
+
+            "white": {
+                "DEFAULT": "hsl(0 0% 100%)",
+                50: "hsl(0 0% 95%)",
+                100: "hsl(0 0% 90%)",
+                150: "hsl(0 0% 85%)",
+                200: "hsl(0 0% 80%)",
+                250: "hsl(0 0% 75%)",
+                300: "hsl(0 0% 70%)"
+            },
+
+            "black": {
+                "DEFAULT": "hsl(0 0% 0%)",
+                50: "hsl(0 0% 5%)",
+                100: "hsl(0 0% 10%)",
+                150: "hsl(0 0% 15%)",
+                200: "hsl(0 0% 20%)",
+                250: "hsl(0 0% 25%)",
+                300: "hsl(0 0% 30%)"
+            }
+        }
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: []
+};
+
+export default config;
