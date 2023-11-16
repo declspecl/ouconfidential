90cfe8d8f61dbcf2d70c02947077bcf31740ea83
declspecl <gavind2559@gmail.com>
2023-11-15 19:37:24 -0500

    added storage bucket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Sidebar from "@/components/Sidebar";
 import SignOutButton from "@/components/SignOutButton";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+    const supabase = createServerComponentClient({ cookies: () => cookies() });
+
+    const { data, error } = await supabase.storage.from("board-profile-pictures").getPublicUrl("ou-logo.png");
+
+    if (error) {
+        console.log(error);
+    }
+
     return (
         <main className="bg-background">
             <Sidebar />
+
+            <div>
+                {data && <img src={data.publicUrl} alt="logo" />}
+            </div>
 
             <SignOutButton />
         </main>
