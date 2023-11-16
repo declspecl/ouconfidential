0bcef904860d5dfcea5ed4c84587c815b12517d9
declspecl <gavind2559@gmail.com>
2023-11-15 21:18:26 -0500

    sidebar logic is good

--- src/components/Sidebar/BoardsLoader.tsx
+++ src/components/Sidebar/BoardsLoader.tsx
@@ -1,27 +1,43 @@
-import { createClient } from "@/backend/utils";
 import { cookies } from "next/headers";
+import { Database } from "@/backend/database.types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default async function BoardsLoader() {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-    const { data: sessionData, error } = await supabase.auth.getSession();
+    const supabase = createServerComponentClient<Database>({ cookies: () => cookies() });
+    
+    const { data: { user: user }, error } = await supabase.auth.getUser();
 
     if (error) {
-        console.error(error);
+        return <p>{JSON.stringify(error, null, 2)}</p>
     }
+    else {
+        if (user) {
+            const { data: joinedBoards, error } = await supabase.from("boards_users")
+                .select("*")
+                .eq("user_uuid", user.id);
 
-    let boards = [];
+            if (error) {
+                return <p>{JSON.stringify(error, null, 2)}</p>
+            }
+            else {
+                const { data: boards, error } = await supabase.from("boards")
+                    .select("*")
+                    .in("board_id", joinedBoards.map(board => board.board_id));
 
-    if (sessionData.session) {
-        const { data } = await supabase.from("boards").select("*").eq("creator_uuid", sessionData.session.user.id);
-
-        boards = data || [];
+                if (error) {
+                    return <p>{JSON.stringify(error, null, 2)}</p>
+                }
+                else {
+                    return (
+                        <ul>
+                            {boards.map(board => <li key={board.board_id}>{board.name}</li>)}
+                        </ul>
+                    );
+                }
+            }
+        }
+        else {
+            return <p>?</p>
+        }
     }
-
-    return (
-        <div className="flex flex-col">
-            <p>yo</p>
-            {boards}
-        </div>
-    );
 }
