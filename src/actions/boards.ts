43ea84372769ecb9a0c9610f106077126a22df53
declspecl <gavind2559@gmail.com>
2023-11-17 18:24:04 -0500

    create board action error handling done

--- src/actions/boards.ts
+++ src/actions/boards.ts
@@ -1,12 +1,37 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { revalidatePath } from "next/cache";
 import { Database } from "@/backend/database.types";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 
 export async function createBoard(formData: FormData) {
+    // define supabase client
     const supabase = createServerActionClient<Database>({ cookies: () => cookies() });
 
+    // define error handling functions
+    async function deleteUploadedPicture(pictureURL: string) {
+        // delete picture from storage
+        const { error: pictureDeleteError } = await supabase.storage.from("board-profile-pictures")
+            .remove([pictureURL]);
+
+        // picture delete failed
+        if (pictureDeleteError)
+            throw pictureDeleteError;
+    }
+
+    async function deleteInsertedBoardRow(name: string) {
+        // delete board row from database
+        const { error: boardDeleteError } = await supabase.from("boards")
+            .delete()
+            .eq("name", name);
+
+        // board delete failed
+        if (boardDeleteError)
+            throw boardDeleteError;
+    }
+
+    // resume
     const session = await supabase.auth.getSession();
 
     if (session.error)
@@ -14,18 +39,23 @@ export async function createBoard(formData: FormData) {
     else if (!session.data.session || !session.data.session.user)
         throw new Error("Session is invalid and/or user is not authenticated.");
 
+    // session is valid, getting form data
     const boardName: string | null = formData.get("boardName") as string | null;
     const boardPicture: File | null = formData.get("boardPicture") as File | null;
 
-    if (!boardName || !boardPicture)
-        throw new Error("Board name and/or picture is missing.");
-
+    // checking if form data is missing / invalid
+    if (!boardName || !boardPicture || boardName.match(/^[a-z0-9-]+$/) === null)
+        throw new Error("Either the board name and/or picture is missing, or the board name is formatted incorrectly.");
+    
+    // uploading picture to storage
     const { data: uploadedPictureURL, error: pictureUploadError} = await supabase.storage.from("board-profile-pictures")
         .upload(boardPicture.name, boardPicture);
 
+    // picture upload failed
     if (pictureUploadError)
         throw pictureUploadError;
 
+    // adding board row to database
     const { error: boardInsertError } = await supabase.from("boards")
         .insert({
             name: boardName,
@@ -33,22 +63,65 @@ export async function createBoard(formData: FormData) {
             creator_uuid: session.data.session.user.id
         });
 
-    if (boardInsertError)
-        throw boardInsertError;
+    // picture successfully uploaded, but board insert failed
+    // must delete uploaded picture
+    if (boardInsertError) {
+        deleteUploadedPicture(uploadedPictureURL.path)
+            .catch((error) => {
+                console.error("Failed to delete uploaded picture after board insert failed.");
+                console.error(error);
+            });
 
+        throw boardInsertError;
+    }
+
+    // reading newly inserted board
     const { data: createdBoardRow, error: readNewBoardError } = await supabase.from("boards")
         .select("*")
         .eq("name", formData.get("boardName") as string);
 
-    if (readNewBoardError)
-        throw readNewBoardError;
+    // reading newly inserted board failed, but board insert succeeded and picture uploaded
+    // must delete inserted board row and uploaded picture
+    if (readNewBoardError) {
+        deleteInsertedBoardRow(boardName)
+            .catch((error) => {
+                console.error("Failed to delete inserted board row after board insert failed.");
+                console.error(error);
+            });
 
+        deleteUploadedPicture(uploadedPictureURL.path)
+            .catch((error) => {
+                console.error("Failed to delete uploaded picture after board insert failed.");
+                console.error(error);
+            });
+
+        throw readNewBoardError;
+    }
+
+    // add creator and board pair to junction table to "join" creator to new board
     const { error: creatorJoinNewBoardError } = await supabase.from("boards_users")
         .insert({
             board_id: createdBoardRow[0].board_id,
             user_uuid: session.data.session.user.id
         });
 
-    if (creatorJoinNewBoardError)
+    // creator join new board failed, but board insert and picture upload succeeded
+    // must delete inserted board row and uploaded picture
+    if (creatorJoinNewBoardError) {
+        deleteInsertedBoardRow(boardName)
+            .catch((error) => {
+                console.error("Failed to delete inserted board row after creator join new board failed.");
+                console.error(error);
+            });
+
+        deleteUploadedPicture(uploadedPictureURL.path)
+            .catch((error) => {
+                console.error("Failed to delete uploaded picture after creator join new board failed.");
+                console.error(error);
+            });
+
         throw creatorJoinNewBoardError;
+    }
+
+    revalidatePath("/");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,8 @@
-import { cookies } from "next/headers";
 import Sidebar from "@/components/Sidebar";
+import { createBoard } from "@/actions/boards";
 import SignOutButton from "@/components/SignOutButton";
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "@/backend/database.types";
-import { NextResponse } from "next/server";
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
 
 export default function Home() {
-    async function handleCreateBoard(formData: FormData) {
-        "use server";
-
-        const supabase = createServerActionClient<Database>({ cookies: () => cookies() });
-
-        const user = await supabase.auth.getUser();
-
-        console.log(formData);
-
-        const profilePicture = formData.get("profilePicture") as File;
-
-        const { data: uploadedUrl, error } = await supabase.storage.from("board-profile-pictures").upload(profilePicture.name, profilePicture);
-
-        const realUploadedURL = supabase.storage.from("board-profile-pictures").getPublicUrl(uploadedUrl!.path);
-
-        if (error) {
-            console.error(error);
-        }
-        else {
-            const { error: ur } = await supabase.from("boards").insert({
-                name: formData.get("boardName") as string,
-                picture_url: realUploadedURL.data.publicUrl,
-                creator_uuid: user.data.user?.id as string
-            });
-
-            console.log(ur);
-
-            const { data: newlyCreatedBoard, error } = await supabase.from("boards").select("*").eq("name", formData.get("boardName") as string);
-
-            if (error) {
-                console.log("its here");
-                console.error(error);
-            }
-            else {
-                const { error: er } = await supabase.from("boards_users").insert({
-                    board_id: newlyCreatedBoard[0].board_id,
-                    user_uuid: user.data.user?.id as string
-                })
-
-                if (er) console.error(er);
-
-                revalidatePath(`/ou/${newlyCreatedBoard[0].name}`);
-            }
-        }
-    }
-
     return (
         <main className="w-full h-full flex flex-row bg-background">
             <Sidebar />
@@ -64,11 +13,11 @@ export default function Home() {
                 <div className="flex flex-col gap-2">
                     <h1>Create board</h1>
 
-                    <form className="flex flex-col text-text" action={handleCreateBoard}>
+                    <form className="flex flex-col text-text" action={createBoard}>
                         <input className="bg-white text-black" type="text" placeholder="Board name" name="boardName" />
 
                         <label htmlFor="profilePicture">Profile picture</label>
-                        <input type="file" accept="image/png,image/jpeg" name="profilePicture" />
+                        <input type="file" accept="image/png,image/jpeg" name="boardPicture" />
 
                         <input type="submit" value="Create" />
                     </form>
